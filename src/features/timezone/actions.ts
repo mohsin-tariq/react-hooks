76a4e6494bc9449115e484eed53dc4c0df146c74
Mohsin Tariq <mynameismohsintariq@gmail.com>
2021-03-09 12:18:27 +0000

    Update actions.ts

--- src/features/timezone/actions.ts
+++ src/features/timezone/actions.ts
@@ -1,5 +1,5 @@
 export const fetchAsync = async (zoneName) => {
-  const url = `/list-time-zone?key=XWSLLPX5RMIZ&format=json&zone=${zoneName}`
+  const url = `/list-time-zone?key=<apikey>&format=json&zone=${zoneName}`  //mention your api key here
   const res = await fetch(url)
   const { zones } = await res.json()
   return zones
